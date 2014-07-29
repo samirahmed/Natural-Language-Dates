6cb91f34cc0301409e27cae4f67279965b546ea3
Samir Ahmed <samirahmed2013@gmail.com>
2014-07-29 15:40:37 -0700

    udpate to simple datetime format

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var http = require('datejs')
 var express = require('express')
-var http = require('http')
 var port = process.env.PORT || 1337;
-
 var app = express();
 
 app.get('/', function(req, res) {
   var querystring = req.query.date
-  res.send( Date.parse(querystring) + '\n');
+  res.send( Date.parse(querystring).toString('d-MMM-yyyy'));
  });
     
 app.listen(port);
